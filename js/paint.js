88e2d3ed1b9eaafd876daa201e5fe167816a0d8a
schtonn <schtonn@163.com>
2022-11-06 09:32:27 +0800

    try cookie

--- js/paint.js
+++ js/paint.js
@@ -30,6 +30,51 @@ function tryClear() {
     tryTime++;
 }
 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    console.log(ca)
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function checkCookie() {
+    var username = getCookie("username");
+    if (username != "") {
+        alert("Welcome again " + username);
+        return username
+    }
+    else {
+        username = prompt("Please enter your name:", "");
+        if (username != "" && username != null) {
+            setCookie("username", username, 365);
+            return username
+        } else {
+            return false
+        }
+    }
+}
+
+function getId(){
+    var cookieId=getCookie('id')
+    if(cookieId!='')return cookieId
+    else{
+        var newId=Math.ceil(Math.random() * 1000000)
+        setCookie('id',newId,365)
+        return newId
+    }
+}
+
 var curVer = 0
 
 function getMap() {
@@ -120,7 +165,8 @@ $(document).ready(function () {
     slider('box-r', 255)
     slider('box-g', 255)
     slider('box-b', 255);
-    var id = Math.ceil(Math.random() * 1000000);
+    var id=getId()
+    console.log('id:'+id)
     var draw = document.getElementById('draw')
     draw.draggable = false
 
