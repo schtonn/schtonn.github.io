359c6e35adc5613cf447e73b25653f6e772c7f48
root <schtonn@163.com>
2022-12-29 10:25:50 +0800

    crawler defense

--- fun/music/gettime.js
+++
@@ -1,7 +0,0 @@
-const fs=require('fs')
-
-var list = ['katyusha', 'e-e-edfe', 'grasswalk', 'where am i going', 'sad', 'tom', 'a#d#cc#c', 'ffgaf', 'bcabc', 'bga#c#', 'cdbc', 'dddc', 'cdd#', 'gddf#dd', 'ag#ag#f#', 'f#g#c#g#a#c#', 'ef#c#ag#', 'friend', 'd#ba#g#g', 'a#agaa#c', 'ggaa#ag', 'a#agaa#c2', 'c#d#fc#c', 'da#gf#g', 'cdd#d#', 'ef#d#c#2', 'bbbdcb', 'efcaa', 'cag#g', 'dc#da', 'ebcdcb', 'aeaba']
-
-for (let i = list.length - 1; i >= 0; i--) {
-    console.log(fs.statSync(list[i]+'.mp3').ctime)
-}
