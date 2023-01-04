8a214711801cb6b899ba47433fe4333a554b410a
root <schtonn@163.com>
2023-01-04 22:55:30 +0800

    add minecraft

---
+++ minecraft/minify.py
@@ -0,0 +1,18 @@
+import os
+import glob
+from PIL import Image
+    
+def thumbnail_pic(path):
+    a=glob.glob(r'image/*.png')
+    print(a)
+    for x in a:
+        name=os.path.join(path,x)
+        im=Image.open(name)
+        im.thumbnail((400,400))
+        print(im.format,im.size,im.mode)
+        im.save('./img/'+name.split('/')[2])
+        print('done!')
+    
+if __name__=='__main__':
+    path='.'
+    thumbnail_pic(path)
