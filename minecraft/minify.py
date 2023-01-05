60d478cdc8b549c01a21ba77962237b359dcab9c
root <schtonn@163.com>
2023-01-05 22:53:07 +0800

    tweak more

--- minecraft/minify.py
+++ minecraft/minify.py
@@ -3,14 +3,14 @@ import glob
 from PIL import Image
     
 def thumbnail_pic(path):
-    a=glob.glob(r'image/*.png')
+    a=glob.glob(r'about/stamp.png')
     print(a)
     for x in a:
         name=os.path.join(path,x)
         im=Image.open(name)
         im.thumbnail((400,400))
         print(im.format,im.size,im.mode)
-        im.save('./img/'+name.split('/')[2])
+        im.save('about/'+name.split('/')[2])
         print('done!')
     
 if __name__=='__main__':
